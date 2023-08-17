8e70ac6ed4957615d8b0b26821184de4f67f9eb5
kasepuu <joel.meeks@outlook.com>
2023-08-17 13:30:48 +0300

    upload

--- upload.sh
+++ upload.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: sh upload.sh <commit_message>"
-  exit 1
-fi
+# if [ -z "$1" ]; then
+#   echo "Usage: sh upload.sh <commit_message>"
+#   exit 1
+# fi
 
-# Commit message provided as the first argument
-commit_message="$1"
+# Default commit message if no argument is provided
+default_commit_message="upload"
+
+# Use the provided argument or the default commit message
+commit_message="${1:-$default_commit_message}"
 
 git add *
 git commit -m "$commit_message"
