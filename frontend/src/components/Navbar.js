eb32f61bb1d0824345a710f8d33b3a28c1348ff6
kasepuu <joel.meeks@outlook.com>
2023-08-19 19:23:04 +0300

    navbar design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,7 @@ import logo from "../logo.svg";
 import { Link } from "react-router-dom";
 
 // react icons: https://react-icons.github.io/react-icons/icons?name=fa
-import {
-  FaAccessibleIcon,
-  FaWineBottle,
-  FaRegAddressCard,
-  FaXRay,
-} from "react-icons/fa";
+import { FaLock, FaMailBulk, FaHome } from "react-icons/fa";
 import SearchContainer from "./SearchContainer";
 
 const Navbar = () => {
@@ -18,39 +13,21 @@ const Navbar = () => {
         <img src={logo} className="NavbarLogo" alt="logo" />
       </Link>
 
-      <SearchContainer />
-
       <div className="links">
-        <>
-          <Link to="/">
-            <FaAccessibleIcon />
-            Home
-          </Link>
-        </>
+        <Link to="/">
+          Home <FaHome />
+        </Link>
 
-        <>
-          {" "}
-          <Link to="/about">
-            <FaXRay />
-            About
-          </Link>
-        </>
+        <Link to="/login">
+          <FaLock /> Login
+        </Link>
 
-        <>
-          <Link to="/login">
-            <FaWineBottle />
-            Login
-          </Link>
-        </>
-
-        <>
-          {" "}
-          <Link to="/register">
-            <FaRegAddressCard />
-            Register
-          </Link>
-        </>
+        <Link to="/register">
+          <FaMailBulk /> Register
+        </Link>
       </div>
+
+      <SearchContainer />
     </nav>
   );
 };
