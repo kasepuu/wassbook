8f48e22388612a6bb4c2296f13dd36a629150fb8
kasepuu <joel.meeks@outlook.com>
2023-10-06 16:47:00 +0300

    kiire cleanup

--- f.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-    npm install --prefix frontend/
-    echo "Dependencies installed."
-
-   echo "Starting frontend server..."
-    npm start --prefix frontend
